fb2f2538c4f076abf3f1216627a9f2f8470c5232
Pranav-Malao <pranavmalao909@gmail.com>
2025-05-14 01:42:32 +0530

    Resolve TypeScript display errors and fix firewall configuration

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,34 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Trending } from '@/Components'
+import { CustomButton } from '@/Components'
+import { signOut } from "@/lib/appwrite";
+import { useGlobalContext } from "@/context/GlobalProvider";
+import { router } from "expo-router";
+
 
 const Profile = () => {
+  const { setUser, setIsLoggedIn } = useGlobalContext();
+
   return (
     <View>
-      <Text>Profile</Text>
+      <CustomButton
+        title='logout'
+        handlePress={() => {
+          const handleSignOut = async () => {
+            try {
+              await signOut();
+              router.push('/sign-in');
+              setUser(null);
+              setIsLoggedIn(false);
+            } catch (error) {
+              console.log(error);
+            }
+          }
+          handleSignOut();
+        }}
+        containerStyles='w-[50%] mt-7'
+      />
     </View>
   )
 }
