a8d0981d4a25f9c850eb1cb66b77825e97ba42d7
Pranav-Malao <pranavmalao909@gmail.com>
2025-05-15 18:32:48 +0530

    Fixed Trending page animations

--- Components/Trending.tsx
+++ Components/Trending.tsx
@@ -1,10 +1,10 @@
 import { View, Text, FlatList, Image, ImageBackground, TouchableOpacity } from 'react-native';
-import React, { useState, useCallback, useMemo } from 'react';
+import React, { useState } from 'react';
 import * as Animatable from 'react-native-animatable';
 import { icons } from '@/constants';
 
 interface Post {
-  $id: number;
+  $id: string;
   thumbnail: string;
 }
 
@@ -22,23 +22,25 @@ const zoomIn = {
 };
 const zoomOut = {
   0: {
-    scale: 1,
+    scale: 1.1,
   },
   1: {
-    scale: 0.9,
+    scale: 1,
   },
 };
 
-const TrendingItem = ({ activeItem, item }: { activeItem: number, item: Post }) => {
+const TrendingItem = ({ activeItem, item }: { activeItem: string, item: Post }) => {
   const [play, setPlay] = useState(false);
-  console.log(activeItem);
+
   return (
     <Animatable.View
       className='mr-5'
-      // animation={activeItem === item.$id ? zoomIn: ''}
-      duration={500}
+      // @ts-expect-error
+      animation={activeItem === item.$id ? zoomIn : zoomOut}
+      duration={400}
+      useNativeDriver={true}
     >
-      { play ? (
+      {play ? (
         <Text>Playing</Text>
       ) : (
         <TouchableOpacity
@@ -59,27 +61,27 @@ const TrendingItem = ({ activeItem, item }: { activeItem: number, item: Post }) 
 }
 
 const Trending = ({ posts }: Props) => {
-  const [activeItem, setActiveItem] = useState(posts[0].$id);
+  const [activeItem, setActiveItem] = useState(posts[0]?.$id);
 
   const viewableItemsChanged = ({ viewableItems }: any) => {
-    if (viewableItems.length > 0) {
-      setActiveItem(viewableItems[0].key);
-      console.log(viewableItems[0].key);
+    if (viewableItems.length > 0) { // ek se zyada items hai tab
+      setActiveItem(viewableItems[0].key); // jo viewable hai (view me dikh raha hai) usko active item bana do jisse vo zoom ho jaye
     }
   };
   return (
     <View>
       <FlatList
-        data={posts}
-        keyExtractor={(item) => item.$id.toString()}
+        data={posts.toReversed()}
+        keyExtractor={(item) => item?.$id}
         renderItem={({ item }) => (
           <TrendingItem activeItem={activeItem} item={item} />
         )}
         onViewableItemsChanged={viewableItemsChanged}
         viewabilityConfig={{
-          viewAreaCoveragePercentThreshold: 70
+          viewAreaCoveragePercentThreshold: 70,
+          minimumViewTime: 100,
         }}
-        // contentOffset={{x: 170, y: 0}}
+        // contentOffset={{ x: 170, y: 0 }}
         horizontal
       />
     </View>
